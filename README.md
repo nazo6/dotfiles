e013d58e05f6c2db7d2c48a1c18bd6bf21f55c09
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-12-17 23:48:48 +0900

    Add configs

---
+++ README.md
@@ -0,0 +1,14 @@
+# dotfiles
+
+## dependencies and so on
+
+### shell(zsh)
+
+- `exa`
+- `zoxide`
+- `nvm`
+- `pnpm`
+
+### wezterm
+
+- Font: `HackGenNerd`
---
+++ src/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab.active_pane.title
+	if #title < max_width then
+		title = title .. string.rep(" ", max_width - #title)
+	elseif #title > max_width then
+		title = title:sub(1, max_width - 4) .. " ..."
+	end
+
+	return title
+end)
+
+return {
+	font = wezterm.font("HackGen Console NF"),
+	default_prog = { [[C:\Users\nazo\AppData\Local\Microsoft\WindowsApps\pwsh.exe]] },
+	keys = {
+		{ key = "l", mods = "ALT", action = "ShowLauncher" },
+	},
+	front_end = "WebGpu",
+	tab_max_width = 20,
+}
