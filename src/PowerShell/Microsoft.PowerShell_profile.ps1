b8d9998dd6b55621c0d3486e463e1c79fd0d73a6
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-12-22 15:17:55 +0900

    Add starship to powershell

--- src/PowerShell/Microsoft.PowerShell_profile.ps1
+++ src/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
+Invoke-Expression (&starship init powershell)
+
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell | Out-String)
