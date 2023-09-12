6aece6e134c7514577f48771bbd20cc3aec769a1
nazo6 <git@nazo6.dev>
2023-09-12 13:21:10 +0900

    Fix

--- src/wezterm/wezterm.lua
+++ src/wezterm/wezterm.lua
@@ -1,22 +1,24 @@
 local wezterm = require("wezterm")
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = tab.active_pane.title
-	if #title < max_width then
-		title = title .. string.rep(" ", max_width - #title)
-	elseif #title > max_width then
-		title = title:sub(1, max_width - 4) .. " ..."
-	end
+  local title = tab.active_pane.title
+  if #title < max_width then
+    title = title .. string.rep(" ", max_width - #title)
+  elseif #title > max_width then
+    title = title:sub(1, max_width - 4) .. " ..."
+  end
 
-	return title
+  return title
 end)
 
 return {
-	font = wezterm.font("PlemolJP Console NF"),
-	default_prog = { "powershell", "pwsh" },
-	keys = {
-		{ key = "l", mods = "ALT", action = "ShowLauncher" },
-	},
-	front_end = "WebGpu",
-	tab_max_width = 20,
+  font = wezterm.font("PlemolJP Console NF"),
+  default_prog = { "nu" },
+  keys = {
+    { key = "l", mods = "ALT", action = "DisableDefaultAssignment" },
+  },
+  front_end = "WebGpu",
+  tab_max_width = 20,
+  window_decorations = "INTEGRATED_BUTTONS",
+  check_for_updates = false
 }
