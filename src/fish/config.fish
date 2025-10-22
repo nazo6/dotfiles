3f81f51919609c335613c7ce7480f48875812a7c
nazo6 <git@nazo6.dev>
2025-10-22 15:44:09 +0900

    add gs command

--- src/fish/config.fish
+++ src/fish/config.fish
@@ -25,6 +25,19 @@ if status is-interactive
     alias ll='ls -la'
   end
 
+  if type -q ghq && type -q fzf
+    function gs --description 'A ghq command wrapper with fzf integration'
+      if test (count $argv) -eq 0
+        set -l repo_path (ghq list | fzf --height 40% --reverse)
+        if test -n "$repo_path"
+          cd (ghq root)/$repo_path
+        end
+      else
+        command ghq $argv
+      end
+    end
+  end
+
   # env
   if type -q nvim
     set -x EDITOR nvim
